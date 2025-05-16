7282e782fb631d7ef7a4c14f951aca5968b6c59e
Natani <otaviosidneiandrade@gmail.com>
2025-05-16 16:27:27 -0300

    Modificado linguagem pt -> eng

---
+++ WebApplication/Client.cs
@@ -0,0 +1,12 @@
+namespace CrudCliente
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+    }
+}
